0499ce3b6f8e1096a126730524288d6d19522dac
Kurnia Zulianti <kurniazulianti22@gmail.com>
2022-07-05 09:03:24 +0700

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+let user = "Anti";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Welcome = (props) => {
+  return(
+    <div>
+      <h1>Selamat datang {props.user}</h1>
+    </div>
+  );
+}
+
+const App = () => {
+  if (user === ""){
+    return <button>Login</button>;
+  }
+  else{
+    return <Welcome user = {user}/>;
+  }
+}
+
+root.render(<App />);
